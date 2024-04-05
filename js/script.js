ae32caddabceab16c4c3025aade5bc3f126d7f92
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-04-05 18:12:19 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Pablo Fiallos
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function enterClicked() {
+  // input
+  const radius = parseInt(document.getElementById("radius").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "V = " + (4/3 * Math.PI * radius**3).toFixed(2) + "u<sup> 3</sup>"
+}
